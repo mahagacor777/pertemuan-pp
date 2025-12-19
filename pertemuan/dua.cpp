8a701a95c1870a2ec2b1f2feabb9541ff5c27a02
mahagacor777 <icalnov755@gmail.com>
2025-12-19 16:21:46 +0700

    Create a simple C++ program
    Add basic C++ program to print 'Hello World'

--- pertemuan/dua.cpp
+++ pertemuan/dua.cpp
@@ -1,1 +1,8 @@
+#include <iostream>     //library untuk dapat menggunakan fungsi cin & cout
+using namespace std;    //agar tidak perlu mengetik std:: pada fungsi cin & cout
 
+int main() {               //function utama
+    cout<<"Hello World";   //mencetak "Hello World" pada terminal
+
+    return 0;      //mengembalikan nilai 0 atau tanda bahwa program telah selesai
+}
